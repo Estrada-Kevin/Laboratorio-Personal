de12fd2cf79152ddb3a1628174431d3445482543
kestrada97a <kestrada97a@gmail.com>
2022-09-01 19:55:30 -0300

    Colecciones 90%

--- Ejercicios/Contador de palabras/Form1.cs
+++ Ejercicios/Contador de palabras/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Contador_de_palabras
@@ -23,35 +19,79 @@ namespace Contador_de_palabras
 
         private void btn_Calcular_Click(object sender, EventArgs e)
         {
-            InsertadorDePalabras(rtxt_Palabras.Text);
-            int count = rtxt_Palabras.Text.Split(' ').Count();
-            MessageBox.Show($"Hay {count} palabras" );
+            Dictionary<string, int> contadorPalabras = ObtenerContadorPalabras();
+            List<KeyValuePair<string, int>> podio = obtenerPodio(contadorPalabras);
+            mostrarPodio(podio);
         }
 
-        public void InsertadorDePalabras(string texto)
-        {
-            char[] separacion = new char[] { ' ', ',', '.', ':', '\t' };
-            palabra.AddRange(texto.Split(separacion, StringSplitOptions.RemoveEmptyEntries));
 
-            foreach (string item in palabra)
+        public Dictionary<string, int> ObtenerContadorPalabras()
+        {
+            string texto = rtxt_Palabras.Text;
+            string[] palabras = texto.Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> contadorPalabras = new Dictionary<string, int>();
+
+            foreach (string item in palabras)
             {
-                if(!diccionario.ContainsKey(item) && item != " ")
+                if (contadorPalabras.ContainsKey(item))
                 {
-                    diccionario.Add(item, 1);
+                    contadorPalabras[item]++;
                 }
                 else
                 {
-                    diccionario[item] += 1;
+                    contadorPalabras.Add(item, 1);
                 }
             }
+
+            return contadorPalabras;
+        }
+
+        private List<KeyValuePair<string, int>> obtenerPodio(Dictionary<string, int> contadorPalabras)
+        {
+            List<KeyValuePair<string, int>> podio = contadorPalabras.ToList(); //transforma el diccionario en una listas con KeyValuePair
+            podio.Sort(compararCantidadRepeticiones);
+
+            return podio;
+        }
+
+        private void mostrarPodio(List<KeyValuePair<string, int>> podio)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (podio.Count == 0)
+            {
+                sb.AppendLine("No se ingresaron palabras");
+            }
+            else
+            {
+                //foreach (KeyValuePair<string, int> item in podio)
+                //{
+                //    sb.AppendLine($"Palabra {item.Key} - Cantidad: {item.Value}");
+                //}
+                for (int i = 0; i < 3 && i<podio.Count; i++)
+                {
+                    KeyValuePair<string,int> item = podio[i];
+                    sb.AppendLine($"Palabra {item.Key} - Cantidad: {item.Value}");
+                }
+
+            }
+            MessageBox.Show(sb.ToString(), "Podio");
+
+
+        }
+
+        private int compararCantidadRepeticiones(KeyValuePair<string, int> primerElemento, KeyValuePair<string, int> segundoElemento)
+        {
+            return segundoElemento.Value - primerElemento.Value;
         }
 
         private void rtxt_Palabras_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Enter)
+            if (e.KeyCode == Keys.Enter)
             {
                 e.Handled = true;
             }
         }
+
     }
 }
