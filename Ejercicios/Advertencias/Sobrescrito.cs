08b4da3ca2d43e3375c53ca2dbd290e791bb87e7
kestrada97a <kestrada97a@gmail.com>
2022-09-09 20:05:07 -0300

    polimorfismo 90%

---
+++ Ejercicios/Advertencias/Sobrescrito.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertencias
+{
+    public abstract class Sobrescrito
+    {
+        protected string miAtributo;
+
+        protected Sobrescrito()
+        {
+            this.miAtributo = "Probar abstractos";
+        }
+
+        public abstract string MiPropiedad
+        {
+            get;
+        }
+
+        public abstract string MiMetodo();
+
+        public override string ToString()
+        {
+            return "¡Este es mi método ToString sobrescrito!"; 
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.GetType() == this.GetType();
+        }
+
+        public override int GetHashCode()
+        {
+            return 1142510181;
+        }
+    }
+}
---
+++ Ejercicios/Calculadora/Rectangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Rectangulo:Figura
+    {
+        private float longitudBase;
+        private float longitudAltura;
+
+        public Rectangulo(float longitudBase, float longitudAltura)
+        {
+            this.longitudBase = longitudBase;
+            this.longitudAltura = longitudAltura;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return (this.longitudBase + this.longitudAltura) * 2;
+        }
+
+        public override double CalcularSuperficie()
+        {
+            return this.longitudBase * this.longitudAltura;
+        }
+
+        public override string Dibujar()
+        {
+            return "Dibujando Rectángulo...";
+        }
+
+
+    }
+}
